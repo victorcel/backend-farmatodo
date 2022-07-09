13238f4e5902724ea606388ea80c15fac954cba8
Victor Elias Barrera Florez <vbarrera@outlook.com>
2022-07-09 15:56:11 -0500

    refactor: Se mejora la presentation del proyecto

--- src/main/java/com/farmatodo/backend/controller/EpisodeController.java
+++ src/main/java/com/farmatodo/backend/controller/EpisodeController.java
@@ -1,25 +1,25 @@
 package com.farmatodo.backend.controller;
 
-import com.farmatodo.backend.services.EpisodeServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.farmatodo.backend.services.EpisodeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-public class ApiRestController {
+@RequestMapping(path = "/v1/episode")
+public class EpisodeController {
 
-    private EpisodeServiceInterface episodeService;
+    private final EpisodeService episodeService;
 
-    public ApiRestController(EpisodeServiceInterface episodeService) {
+    public EpisodeController(EpisodeService episodeService) {
         this.episodeService = episodeService;
     }
 
-    @GetMapping(path = "/v1/episode/{param}")
+    @GetMapping(path = "/{param}")
 
     public ResponseEntity<Map<String, Object>> getEpisode(@PathVariable("param") String param) {
         return episodeService.getEpisode(Integer.parseInt(param));
--- src/main/java/com/farmatodo/backend/services/EpisodeService.java
+++ src/main/java/com/farmatodo/backend/services/EpisodeService.java
@@ -3,6 +3,7 @@ package com.farmatodo.backend.services;
 import com.farmatodo.backend.entity.Episode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,22 +12,22 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Map;
 
 @Service
-public class EpisodeService implements EpisodeServiceInterface {
+public class EpisodeService {
 
     private final static Logger logger = LoggerFactory.getLogger(EpisodeService.class);
 
-    @Override
+    @Value("${url.episodio}")
+    private String url_episodio;
+
     public ResponseEntity<Map<String, Object>> getEpisode(int Id) {
         try {
-            final String uri = "https://rickandmortyapi.com/api/episode/" + Id;
+            final String uri = url_episodio + Id;
             RestTemplate restTemplate = new RestTemplate();
             Episode result = restTemplate.getForObject(uri, Episode.class);
-            return ResponseEntity.status(HttpStatus.OK).body(result.toJson());
+            return ResponseEntity.status(HttpStatus.OK).body(result != null ? result.toJson() : null);
         } catch (Exception exception) {
             logger.error(exception.getMessage());
             return ResponseEntity.status(HttpStatus.FOUND).body(Map.of("error", exception.getMessage()));
         }
-
-
     }
 }
